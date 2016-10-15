964b44c669fe9943b1c7a6ee49ffc68a922bf77f
Yumiko Kohkawa <sa136002@bbt.ohmae.ac.jp>
2016-10-15 18:51:01 +0000

    microposts 課題１

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
+  before_action :set_params, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
   def show # 追加
-   @user = User.find(params[:id])
+   # @user = User.find(params[:id])
   end
   
   def new
@@ -10,10 +13,25 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Sample App!" # ここを追加
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user # ここを修正
     else
       render 'new'
+    end
+  end
+  
+  def edit
+   # @user = User.find(params[:id])
+   # render 'edit'
+  end
+  
+  def update
+   # @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Updated your Plofile"
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
 
@@ -21,7 +39,14 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation, :location)
   end
   
+  def set_params
+    @user = User.find(params[:id])
+  end
+
+  def correct_user
+    redirect_to root_path if @user != current_user
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161007115700) do
+ActiveRecord::Schema.define(version: 20161013124221) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20161007115700) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "location"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
