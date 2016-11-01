c42c40465612beb38801448a37b4db3c010e46cf
Yumiko Kohkawa <sa136002@bbt.ohmae.ac.jp>
2016-11-01 08:47:20 +0000

    日本語化２

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -19,7 +19,7 @@ class MicropostsController < ApplicationController
     flash[:success] = "Micropost deleted"
     redirect_to request.referrer || root_url
   end
- 
+  
   private
   def micropost_params
     params.require(:micropost).permit(:content)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
 
   def show # 追加
    # @user = User.find(params[:id])
-   @microposts = @user.microposts.order(created_at: :desc)
+   @microposts = @user.microposts.order(created_at: :desc).page(params[:page]).per(3)
   end
   
   def new
