5d97f51b3565ff4b0e181fa5906736f99edcab15
Yumiko Kohkawa <sa136002@bbt.ohmae.ac.jp>
2016-11-10 12:00:35 +0000

    課題提出（微調整２）（未達　アバター画像アップロード）

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -20,37 +20,37 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  #def store_dir
+  #  "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  #end
 
 
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  def default_url
-   # For Rails 3.1+ asset pipeline compatibility:
-   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  #def default_url
+  # # For Rails 3.1+ asset pipeline compatibility:
+  # # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   
-   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  end
+  # "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  #end
 
   # Process files as they are uploaded:
-  process :scale => [200, 300]
+  #process :scale => [200, 300]
   #
   # def scale(width, height)
   #   # do something
   # end
 
   # Create different versions of your uploaded files:
-  version :thumb do
-    process :resize_to_fit => [50, 50]
-  end
+  #version :thumb do
+  #  process :resize_to_fit => [50, 50]
+  #end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  def extension_white_list
-    %w(jpg jpeg gif png)
-  end
+  #def extension_white_list
+  #  %w(jpg jpeg gif png)
+  #end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
@@ -58,10 +58,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
   #   "something.jpg" if original_filename
   # end
 
-  def filename
-    time = Time.now
-    name = time.strftime('%Y%m%d%H%M%S') + '.jpg'
-    name.downcase
-  end
+  #def filename
+  #  time = Time.now
+  #  name = time.strftime('%Y%m%d%H%M%S') + '.jpg'
+  #  name.downcase
+  #end
 
 end
